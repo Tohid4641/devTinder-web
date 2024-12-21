b854fefef9558c285ebbff9a04acfb52a0680896
Tauhid <sktohid111@gmail.com>
2024-12-21 12:47:44 +0530

    connections page done

---
+++ src/components/Connections.jsx
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionSlice";
+import { BASE_URL } from "../utils/constants";
+
+const Connections = () => {
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const connections = useSelector((store) => store.connections) || [];
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/api/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res?.data?.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (connections.length === 0) {
+      fetchConnections();
+    }
+  }, []);
+
+  return (
+    <div className="text-center flex flex-col justify-center">
+      <div className="text-xl  font-bold my-4">
+        <h1>Connections</h1>
+      </div>
+      <div className="mx-auto overflow-x-auto w-full sm:w-1/2 bg-base-300">
+        <table className="table">
+          <tbody>
+            {/* row 1 */}
+            {connections?.length == 0 ? (
+              <tr>
+                <td className="m-5">No connections found</td>
+              </tr>
+            ) : (
+              connections.map((connection) => (
+                <tr key={connection._id} className="hover:bg-base-200">
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar ">
+                        <div className="mask mask-squircle h-12 w-12 rounded-full">
+                          <img
+                            src={connection.photoUrl}
+                            alt="connection photo"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{`${connection.firstName} ${connection.lastName}`}</div>
+                        <div className="text-sm opacity-50">
+                          {connection.about && connection.about}
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Connections;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,33 +6,37 @@ import { removeUser } from "../utils/userSlice";
 import { useEffect } from "react";
 
 const Navbar = () => {
-  const user = useSelector((store) => store.user)
+  const user = useSelector((store) => store.user);
+  const connections = useSelector((store) => store.connections);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
   const handleLogout = async () => {
     try {
-      await axios.post(BASE_URL + '/api/auth/logout', {}, {
-        withCredentials: true
-      });
+      await axios.post(
+        BASE_URL + "/api/auth/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
       dispatch(removeUser());
-      return navigate('/login');
+      return navigate("/login");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
-  
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <Link to='/' className="btn btn-ghost text-xl">DavTinder</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          DavTinder
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
-          <Link to='/profile'>Welcome, {user.firstName}</Link>
+          <Link to="/profile">Welcome, {user.firstName}</Link>
           <div className="dropdown dropdown-end">
             <div
               tabIndex={0}
@@ -40,10 +44,7 @@ const Navbar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src={user.photoUrl}
-                />
+                <img alt="Tailwind CSS Navbar component" src={user.photoUrl} />
               </div>
             </div>
             <ul
@@ -51,9 +52,15 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <Link to='/profile' className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
-                  <span className="badge">New</span>
+                  {/* <span className="badge">New</span> */}
+                </Link>
+              </li>
+              <li>
+                <Link to="/connections" className="justify-between">
+                  Connections
+                  <span className="badge">{connections ? connections.length : 0}</span>
                 </Link>
               </li>
               <li>
